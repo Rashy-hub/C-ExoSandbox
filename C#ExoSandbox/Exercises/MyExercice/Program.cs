0c6e6cc37c3136d847cb726eaf8e8715655f8b00
Rashy-hub <yagoubi.r@gmail.com>
2025-03-01 00:59:52 +0100

    feat: add an exercices as an example

---
+++ C#ExoSandbox/Exercises/MyExercice/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
